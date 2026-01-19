034672a15b893ca76d2f2439faa4ed4bf3465180
Jan1406 <95484557+Jan1406@users.noreply.github.com>
2026-01-19 21:33:26 +0100

    Fix router basename for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -13,12 +13,12 @@ const App = () => (
     <TooltipProvider>
       <Sonner />
       <Toaster />
-      <BrowserRouter>
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
-        </Routes>
+       </Routes>
       </BrowserRouter>
     </TooltipProvider>
   </QueryClientProvider>
